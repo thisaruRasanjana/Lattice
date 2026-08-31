60374df3ea44a25df426ab47df3e5b8355412c10
Thisaru Ramanayaka <thisarurasanjana242@gmail.com>
2026-09-01 00:17:25 +0530

    implement boundary modes and live neighbor counting

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
+enum BoundaryMode{
+    Wrap,
+    Fixed,
+}
+
 struct Grid {
     width: usize,
     height: usize,
+    boundary_mode: BoundaryMode,
     current: Vec<u8>,
     next: Vec<u8>,
 }
 
 impl Grid {
     // 1. Initialize the grid
-    fn new(width: usize, height: usize) -> Self {
+    fn new(width: usize, height: usize, boundary_mode: BoundaryMode) -> Self {
         let total_size = width * height;
         
         Grid {
             width,
             height,
+            boundary_mode,
             // vec![0; size] creates an array of zeros of that exact size
             current: vec![0; total_size],
             next: vec![0; total_size],
@@ -23,10 +30,50 @@ impl Grid {
     fn get_index(&self, x: usize, y: usize) -> usize {
         y * self.width + x
     }
+
+        fn live_neighbor_count(&self, x: usize, y: usize) -> u8 {
+        let mut count = 0;
+
+        // The 8 directions around a cell (dx, dy)
+        let offsets: [(isize, isize); 8] = [
+            (-1, -1), (0, -1), (1, -1), // Top row
+            (-1,  0),          (1,  0), // Middle row
+            (-1,  1), (0,  1), (1,  1), // Bottom row
+        ];
+
+        for (dx, dy) in offsets {
+            match self.boundary_mode {
+                BoundaryMode::Wrap => {
+                    // 1. Calculate the raw neighbor coordinates
+                    let nx = (x as isize + dx).rem_euclid(self.width as isize) as usize;
+                    let ny = (y as isize + dy).rem_euclid(self.height as isize) as usize;
+
+                    // 2. Convert to index and add to `count`
+                    let index = self.get_index(nx, ny);
+                    count += self.current[index];
+
+                }
+                BoundaryMode::Fixed => {
+                    // 1. Calculate the raw neighbor coordinates
+                    let nx = x as isize + dx;
+                    let ny = y as isize + dy;
+
+                    // 2. Check if the neighbor is within bounds
+                    if nx >= 0 && nx < self.width as isize && ny >= 0 && ny < self.height as isize {
+                        let index = self.get_index(nx as usize, ny as usize);
+                        count += self.current[index];
+                    }
+                }
+            }
+        }
+
+        count
+    }
+
 }
 
 fn main() {
-    let grid = Grid::new(5, 5);
+    let grid = Grid::new(5, 5, BoundaryMode::Wrap);
     println!("Grid initialized with {} cells!", grid.current.len());
 }
 
@@ -36,7 +83,7 @@ mod tests {
 
     #[test]
     fn test_get_index() {
-        let grid = Grid::new(5, 5);
+        let grid = Grid::new(5, 5, BoundaryMode::Wrap);
         
         // Top-left corner
         assert_eq!(grid.get_index(0, 0), 0);
